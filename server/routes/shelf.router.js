d1a1a2d07835384f34143b743806c8999ca539a9
Jonathon Baxendell <j.bax626@gmail.com>
2022-10-14 10:25:05 -0500

    set up POST route

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,13 +6,20 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  let queryText = `SELECT * FROM "item"`;
-  pool.query(queryText).then((result) => {
-    res.send(result.rows);
-  }).catch((error) => {
-    console.log(error);
-    res.sendStatus(500);
-  });
+  console.log(req.user)
+  if(req.isAuthenticated()){
+    let queryText = `SELECT * FROM "item"`;
+    pool.query(queryText).then((result) => {
+      res.send(result.rows);
+    }).catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+
+  } else {
+    res.sendStatus(403) // Forbidden
+  }
+
 });
 
 /**
@@ -20,6 +27,22 @@ router.get('/', (req, res) => {
  */
 router.post('/', (req, res) => {
   // endpoint functionality
+  console.log('/shelf POST route');
+  console.log(req.body);
+  console.log('is authenticated?', req.isAuthenticated());
+  console.log('user', req.user);
+  if (req.isAuthenticated()) {
+    const queryText = `INSERT INTO "item" ("description", "image_url", "user_id")
+                        VALUES ($1, $2, $3)`;
+  pool.query(queryText, [ req.body.item ,req.body.url , req.user.id])
+  .then(() => {
+    res.sendStatus(201);
+  }).catch((e) => {
+    res.sendStatus(500);
+  })
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 /**
--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -4,9 +4,12 @@ import './ShelfPage.css';
 
 function ShelfPage() {
   const [shelfList, setShelfList] = useState([]);
+  const [item, setItem] = useState('');
+  const [url, setUrl] = useState('');
 
   useEffect(() => {
     fetchPets();
+    
   }, []);
 
   const fetchPets = () => {
@@ -17,11 +20,29 @@ function ShelfPage() {
       alert('Something went wrong.');
     });
   }
+  
+  const addItem = (e) => {
+    e.preventDefault();
+    axios.post('/api/shelf', {item: item, url: url})
+    .then(() => {
+      fetchPets();
+  }).catch((e) => {
+      console.log(e);
+      alert('something went wrong in  add item')
+  })
+}
 
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <div>
+      <form onSubmit={addItem}>
+          <input onChange={(e) => setItem(e.target.value)} value={item} type="text" />
+          <input onChange={(e) => setUrl(e.target.value)} value={url} type="text" />
+          <input type="submit" />     
+      </form>
+      </div>
       {
         shelfList.length === 0 && (
           <div>No items on the shelf</div>
